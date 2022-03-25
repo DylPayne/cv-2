5a7261f3546ea175b9b51b19537e38ed1737b500
Dylan Payne <payne.r.dylan@gmail.com>
2022-03-25 10:59:10 +0200

    fixing SSR issues

--- pages/index.js
+++ pages/index.js
@@ -32,12 +32,57 @@ import {
   Send,
 } from "tabler-icons-react";
 import { useForm } from "@mantine/hooks";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const useWindowSize = () => {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== "undefined") {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+};
 
 export default function Home() {
   const [scroll, scrollTo] = useWindowScroll();
 
-  const isTabletOrMobile = useMediaQuery({ maxWidth: 1000 });
+  // const isTabletOrMobile = useMediaQuery({ maxWidth: 1000 });
+  const size = useWindowSize();
+  const isTabletOrMobile = size.width < 1000;
+
+  useEffect(() => {
+    console.log(size);
+  }, [size]);
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(1);
+  }, [count]);
 
   const form = useForm({
     initialValues: {
